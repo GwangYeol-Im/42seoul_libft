91175cf72ca52bec2959c0836ad26beef844ce38
Kyle <mkitigy@gmail.com>
2020-09-28 21:22:36 +0900

    add header

---
+++ libft.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imgwang-yeol <imgwang-yeol@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/28 13:43:02 by imgwang-yeo       #+#    #+#             */
+/*   Updated: 2020/09/28 21:06:01 by imgwang-yeo      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <stdio.h>
+#include <stddef.h>
+
+int			ft_isalpha(int c);
+int			ft_isdigit(int c);
+int			ft_isascii(int c);
+int			ft_isalnum(int c);
+int			ft_tolower(int c);
+int			ft_toupper(int c);
+
+size_t		ft_strlen(const char *s);
+
+#endif
