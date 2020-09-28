c3aa26d83a35e3ed01d659ec928c49d93bd4d5dc
Kyle <mkitigy@gmail.com>
2020-09-28 23:24:14 +0900

    add strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imgwang-yeol <imgwang-yeol@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/28 22:15:12 by imgwang-yeo       #+#    #+#             */
+/*   Updated: 2020/09/28 23:19:07 by imgwang-yeo      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t		ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	int		idx;
+	int		len;
+	int		dst_len;
+	int		src_len;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dst_len >= dstsize)
+		return (src_len + dstsize);
+	idx = dst_len;
+	while (idx < dstsize - 1)
+	{
+		if (*src == '\0')
+		{
+			dst[idx] = '\0';
+			return (dst_len + src_len);
+		}
+		dst[idx++] = *src++;
+	}
+	dst[idx] = '\0';
+	return (dst_len + src_len);
+}
