153d2369e6e7679212c33c808034d4b5f1489845
Kyle <mkitigy@gmail.com>
2020-09-28 22:08:54 +0900

    add strlcpy.c

---
+++ ft_strlcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imgwang-yeol <imgwang-yeol@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/28 21:31:32 by imgwang-yeo       #+#    #+#             */
+/*   Updated: 2020/09/28 22:03:44 by imgwang-yeo      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t		strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	int		idx;
+	int		len;
+
+	len = ft_strlen(src);
+	idx = 0;
+	while (idx < dstsize - 1)
+	{
+		if (src[idx] == '\0')
+		{
+			dst[idx] = '\0';
+			return (len);
+		}
+		dst[idx] = src[idx];
+		idx++;
+	}
+	dst[dstsize - 1] = '\0';
+	return (len);
+}
