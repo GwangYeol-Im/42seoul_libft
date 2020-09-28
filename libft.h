494959a33239462f2401464e126c57af2fbc8b1c
Kyle <mkitigy@gmail.com>
2020-09-28 22:08:18 +0900

    header_add isprint, strlcpy

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: imgwang-yeol <imgwang-yeol@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 13:43:02 by imgwang-yeo       #+#    #+#             */
-/*   Updated: 2020/09/28 21:06:01 by imgwang-yeo      ###   ########.fr       */
+/*   Updated: 2020/09/28 22:05:11 by imgwang-yeo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,11 @@ int			ft_isalpha(int c);
 int			ft_isdigit(int c);
 int			ft_isascii(int c);
 int			ft_isalnum(int c);
+int			ft_isprint(int c);
 int			ft_tolower(int c);
 int			ft_toupper(int c);
 
 size_t		ft_strlen(const char *s);
+size_t		strlcpy(char *dst, const char *src, size_t dstsize);
 
 #endif
