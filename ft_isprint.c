dc86fb2fb24a32a163eca2c7cea8585c39a17290
Kyle <mkitigy@gmail.com>
2020-09-28 22:08:33 +0900

    modify isprint

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,11 +6,11 @@
 /*   By: imgwang-yeol <imgwang-yeol@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 13:59:41 by imgwang-yeo       #+#    #+#             */
-/*   Updated: 2020/09/28 14:11:16 by imgwang-yeo      ###   ########.fr       */
+/*   Updated: 2020/09/28 21:30:20 by imgwang-yeo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		isprint(int c)
+int		ft_isprint(int c)
 {
-	
+	return (c >= 32 && c <= 126);
 }
