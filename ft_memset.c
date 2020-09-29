6473c5f22db997d950d46674747577afb20bb4b6
Kyle <mkitigy@gmail.com>
2020-09-29 10:26:19 +0900

    add memset

---
+++ ft_memset.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imgwang-yeol <imgwang-yeol@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/29 09:54:55 by imgwang-yeo       #+#    #+#             */
+/*   Updated: 2020/09/29 10:24:40 by imgwang-yeo      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void				*ft_memset(void *ptr, int value, size_t num)
+{
+	unsigned char	*temp;
+
+	temp = ptr;
+	while (num--)
+	{
+		*temp++ = (unsigned char)value;
+	}
+	return (ptr);
+}
