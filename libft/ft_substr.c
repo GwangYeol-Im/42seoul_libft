4a02662d69ba135607b0593840561f7850c7aa29
Kyle <mkitigy@gmail.com>
2020-10-01 10:34:48 +0900

    correct substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: gim <gim@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 11:23:57 by gim               #+#    #+#             */
-/*   Updated: 2020/09/30 14:14:38 by gim              ###   ########.fr       */
+/*   Updated: 2020/10/01 10:34:23 by gim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,23 @@ char		*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*str;
 	size_t	idx;
 
-	if (start > ft_strlen(s))
-		return (0);
+	if (start > ft_strlen(s) || len <= 0)
+	{
+		sub = malloc(0);
+		return (sub);
+	}
 	sub = malloc(sizeof(char) * (len + 1));
 	if (!sub)
 		return (0);
-	str = (char *)s;
-	while (start--)
-		str++;
+	str = (char *)s + start;
 	idx = 0;
-	while (idx <= len)
+	while (idx < len)
 	{
-		sub[idx] = str[idx];
 		if (!str[idx])
 			return (sub);
+		sub[idx] = str[idx];
 		idx++;
 	}
+	sub[idx] = '\0';
 	return (sub);
 }
