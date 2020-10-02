a8f237a7dde233813b2edefb702a7702ffd7ed3b
Gwang yeol Im <gim@c1r2s3.42seoul.kr>
2020-10-02 19:49:18 +0900

    correct substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: gim <gim@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 11:23:57 by gim               #+#    #+#             */
-/*   Updated: 2020/10/02 18:58:31 by gim              ###   ########.fr       */
+/*   Updated: 2020/10/02 19:48:32 by gim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ char		*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*sub;
 	char	*str;
 	size_t	s_len;
-	size_t	idx;
 
 	s_len = ft_strlen(s);
 	if (start > ft_strlen(s) || len < 0)
@@ -29,6 +28,6 @@ char		*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!sub)
 		return (0);
 	str = (char *)s + start;
-	ft_strlcpy(sub, str, len);
+	ft_strlcpy(sub, str, len + 1);
 	return (sub);
 }
