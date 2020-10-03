3b3fa28dd046550bc7ae4d829472782e8f3ef7a6
Kyle <mkitigy@gmail.com>
2020-10-03 14:41:32 +0900

    modify function

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: gim <gim@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 11:23:57 by gim               #+#    #+#             */
-/*   Updated: 2020/10/03 13:48:42 by gim              ###   ########.fr       */
+/*   Updated: 2020/10/03 14:41:21 by gim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char		*ft_substr(char const *s, unsigned int start, size_t len)
 
 	s_len = ft_strlen(s);
 	if (start > s_len || len < 0)
-		return (sub = malloc(0));
+		return (ft_strdup(""));
 	sub = malloc(sizeof(char) * (len + 1));
 	if (!sub)
 		return (0);
